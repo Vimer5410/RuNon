3fcb5188a569eba34ea283aedb614a501885080b
Vimer5410 <gleb.dubinin@rambler.ru>
2025-08-10 12:20:26 +0300

    first commit

---
+++ RuNon-Client/Hubs/SimpleHub.cs
@@ -0,0 +1,12 @@
+﻿namespace RuNon_Client.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+
+public class SimpleHub : Hub
+{
+    public async Task SendToClient(string clientID,string message)
+    {
+        Console.WriteLine($"Sending to {clientID}: {message}");
+        await Clients.Client(clientID).SendAsync("ReceiveMessage", message);
+    }
+}
