afdbadd7815db7710a45c32520bb0a6a5f5aa2eb
Vimer5410 <gleb.dubinin@rambler.ru>
2025-10-17 02:08:56 +0300

    improved the UI, made a mobile UI fix, and fixed several backend bugs (such as an empty message, delayed receiving an IPEndPoint)

--- RuNon-Client/obj/Debug/net9.0/RuNon-Client.AssemblyInfo.cs
+++ RuNon-Client/obj/Debug/net9.0/RuNon-Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RuNon-Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae7e0fedd4ed14856109459d52aad6dc38a94f49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24051784bda5da41bad4af4baa23900ceb5e092d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RuNon-Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RuNon-Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
