4ea276b1955e3812a4f7f8c945a39520aeb503af
Vimer5410 <gleb.dubinin@rambler.ru>
2025-11-16 20:26:47 +0300

    update algorithm logic and add serilog(not everywhere)

--- RuNon-Client/Services/MatchMakingService.cs
+++ RuNon-Client/Services/MatchMakingService.cs
@@ -1,41 +1,69 @@
+using Serilog;
+
 namespace RuNon_Client.Services;
 
 public class MatchMakingService
 {
-    public static List<(string, DateTime)> PeopleInQuery = new List<(string, DateTime)>();
+    public static List<(string, DateTime, string, string, string, string)> PeopleInQuery = new 
+        List<(string, DateTime,string, string, string, string)>();
     public static (string?, string?) pair;
 
-    public void AddToQueue(string userId)
+    public void AddToQueue(string userId,  string UserGender, string UserAge, string SearchGender, string SearchAge)
     {
-        PeopleInQuery.Add((userId,DateTime.Now));
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"Пользователь {userId} был добавлен в очередь поиска");
+        PeopleInQuery.Add((userId,DateTime.Now, UserGender, UserAge, SearchGender, SearchAge));
+        
+        Log.Information("[Match-Making] Пользователь {UserId} : {UserGender}, {UserAge} ищет {SearchGender}, {SearchAge}",
+            userId,
+            UserGender,
+            UserAge,
+            SearchGender,
+            SearchAge);
     }
 
     public void RemoveFromQueue(string userId)
     {
         PeopleInQuery.RemoveAll(e => e.Item1 == userId);
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"Пользователь {userId} был удален из оереди поиска");
+        Log.Information("[Match-Making] Пользователь {userId} был удален из очереди поиска", userId);
+        
     }
 
-    public (string?, string?) SearchCommand()
+    public (string?, string?) SearchCommand(string userId)
     {
-        if (PeopleInQuery.Count<2)
+        var sortedUsers = PeopleInQuery
+            .GroupBy(e => e.Item1) 
+            .Select(g => g.First()) 
+            .OrderBy(e => e.Item2) 
+            .ToList();
+        if (sortedUsers.Count<2)
         {
+            Log.Information("[Match-Making] Недостаточно пользователей для поиска");
             
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Недостаточно пользователей для поиска");
         }
         else
         {
-            var sortedUsers = PeopleInQuery.OrderBy(e => e.Item2).ToList();
+            List<(string, DateTime, string, string, string, string)> MaleInQuery =
+                sortedUsers.Where(e => e.Item5 == "М").ToList();
             
-            pair.Item1 = sortedUsers[0].Item1;
-            pair.Item2 = sortedUsers[1].Item1;
+            List<(string, DateTime, string, string, string, string)> FemaleInQuery =
+                sortedUsers.Where(e => e.Item5 == "Ж").ToList();
+            
+            if (sortedUsers[0].Item1!=sortedUsers[1].Item1)
+            {
+                pair.Item1 = sortedUsers[0].Item1;
+                pair.Item2 = sortedUsers[1].Item1;
+                
+                Log.Information("[Match-Making] Найдена пара: {pair}", pair);
 
-            Console.WriteLine($"Найдена пара: {pair}");
-            
+                PeopleInQuery.RemoveAll(e => e.Item1==pair.Item1);
+                PeopleInQuery.RemoveAll(e => e.Item1==pair.Item2);
+            }
+            else
+            {
+
+                Log.Information("[Match-Making] Попытка соединения юзера самим с собой");
+            }
+
+
         }
         return pair;
     }
