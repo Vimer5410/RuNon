f50344f2f5b687bac0cd4a6951cabb39062fcc22
Vimer5410 <gleb.dubinin@rambler.ru>
2025-12-06 16:56:41 +0300

    add ECDH, HPKE benchmarks

--- Benchmarks/BenchmarkDotNet.Artifacts/results/FullCryptoBenchmarks-report-github.md
+++ Benchmarks/BenchmarkDotNet.Artifacts/results/FullCryptoBenchmarks-report-github.md
@@ -8,9 +8,15 @@ Intel Core i7-9700K CPU 3.60GHz (Coffee Lake), 1 CPU, 8 logical and 8 physical c
 
 
 ```
-| Method                            | Mean            | Error           | StdDev          | Rank | Gen0   | Allocated |
-|---------------------------------- |----------------:|----------------:|----------------:|-----:|-------:|----------:|
-| &#39;Aes Encrypt Only&#39;                |        795.0 ns |         2.14 ns |         2.00 ns |    1 | 0.0658 |     416 B |
-| &#39;Aes Encrypt With Key Generation&#39; |      1,432.7 ns |         3.22 ns |         2.86 ns |    2 | 0.1335 |     848 B |
-| &#39;Rsa Encrypt Only&#39;                |     16,899.9 ns |       113.38 ns |       100.51 ns |    3 | 0.0305 |     280 B |
-| &#39;Rsa Encrypt With Key Generation&#39; | 62,013,218.9 ns | 1,822,215.67 ns | 5,315,489.04 ns |    4 |      - |    2147 B |
+| Method                                      | Mean         | Error        | StdDev       | Rank | Gen0     | Gen1     | Gen2     | Allocated |
+|-------------------------------------------- |-------------:|-------------:|-------------:|-----:|---------:|---------:|---------:|----------:|
+| &#39;Rsa Encrypt Only&#39;                          |     17.60 μs |     0.328 μs |     0.459 μs |    1 |   0.0305 |        - |        - |     280 B |
+| &#39;ECDH Encrypt Only&#39;                         |  1,016.88 μs |    19.391 μs |    17.190 μs |    2 |        - |        - |        - |     777 B |
+| &#39;HPKE AES+RSA Encrypt Only&#39;                 |  1,243.47 μs |     3.647 μs |     3.233 μs |    3 | 248.0469 | 248.0469 | 248.0469 | 1051355 B |
+| &#39;Aes Encrypt With Key Generation&#39;           |  1,273.82 μs |    18.367 μs |    16.282 μs |    3 | 248.0469 | 248.0469 | 248.0469 | 1051410 B |
+| &#39;Aes Encrypt Only&#39;                          |  1,343.73 μs |    23.611 μs |    20.931 μs |    4 | 248.0469 | 248.0469 | 248.0469 | 1051004 B |
+| &#39;HPKE AES+ECDH Encrypt Only&#39;                |  2,270.10 μs |     4.946 μs |     4.385 μs |    5 | 246.0938 | 246.0938 | 246.0938 | 1052070 B |
+| &#39;HPKE AES+ECDH Encrypt With Key Generation&#39; |  4,572.87 μs |    16.318 μs |    14.465 μs |    6 | 242.1875 | 242.1875 | 242.1875 | 1054138 B |
+| &#39;ECDH Encrypt With Key Generation&#39;          |  5,849.60 μs |     7.692 μs |     6.424 μs |    7 |        - |        - |        - |    2893 B |
+| &#39;HPKE AES+RSA Encrypt With Key Generation&#39;  | 62,924.36 μs | 1,844.895 μs | 5,381.644 μs |    8 | 125.0000 | 125.0000 | 125.0000 | 1052606 B |
+| &#39;Rsa Encrypt With Key Generation&#39;           | 63,014.30 μs | 1,659.558 μs | 4,814.679 μs |    8 |        - |        - |        - |    2144 B |
--- Benchmarks/FullCryptoBenchmarks.cs
+++ Benchmarks/FullCryptoBenchmarks.cs
@@ -2,15 +2,22 @@ using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 using System.Security.Cryptography;
 using System.Text;
+using BenchmarkDotNet.Order;
 
 [MemoryDiagnoser]
+[Orderer (SummaryOrderPolicy.FastestToSlowest)]
 [RankColumn] 
 public class FullCryptoBenchmarks
 {
     
     private Aes aes;
     private RSA rsa;
-    private byte[] data;
+    private ECDiffieHellman alice;
+    private ECDiffieHellman bob;
+    private ECDiffieHellmanPublicKey bobPublicKey;
+    private byte[] dsaSignature;
+    private byte[] AESdata;
+    private byte[] RSAdata;
     private byte[] iv;
     
     
@@ -19,11 +26,10 @@ public class FullCryptoBenchmarks
     public void Setup()
     {
         //AES
-        // подготовка данных которые будут тестироваться
-        data = new byte[200]; // средняя длина шифр-текста
-        new Random().NextBytes(data);
-
         // настройка общих Aes параметров
+        // подготовка данных которые будут тестироваться
+        AESdata = new byte[1024*1024]; // средняя длина шифр-текста
+        new Random().NextBytes(AESdata);
         aes = Aes.Create();
         aes.GenerateKey();
         aes.GenerateIV();
@@ -33,6 +39,9 @@ public class FullCryptoBenchmarks
         
         //RSA
         //отдельно генерируем и импортируем ключи
+        // подготовка данных которые будут тестироваться
+        RSAdata = new byte[32]; // средняя длина шифр-текста
+        new Random().NextBytes(RSAdata);
         rsa = RSA.Create(2048);
         RSAParameters publickey = rsa.ExportParameters(false);
         RSAParameters privatekey = rsa.ExportParameters(true);
@@ -40,14 +49,21 @@ public class FullCryptoBenchmarks
         rsa.ImportParameters(publickey);
         
         
+        //ECDH
+        // подготовка данных которые будут тестироваться
+        alice=ECDiffieHellman.Create(ECCurve.NamedCurves.nistP384);
+        bob= ECDiffieHellman.Create(ECCurve.NamedCurves.nistP384);
+        bobPublicKey = bob.PublicKey;
+        alice.DeriveKeyFromHash(bobPublicKey, HashAlgorithmName.SHA256);
     }
 
     // тест AES только с шифрованием текста(без генерации ключей)
     [Benchmark (Description = "Aes Encrypt Only")]
     public byte[] AesEncryptOnly()
     {
-        return aes.EncryptCbc(data, iv);
+        return aes.EncryptCbc(AESdata, iv);
     }
+    
     // тест AES с шифрованием и генерацией ключей
     [Benchmark(Description = "Aes Encrypt With Key Generation")]
     public byte[] AesEncryptWithKeyGeneration()
@@ -56,17 +72,18 @@ public class FullCryptoBenchmarks
         aes.GenerateKey();
         aes.GenerateIV();
         iv = aes.IV;
-        return aes.EncryptCbc(data, iv);
+        return aes.EncryptCbc(AESdata, iv);
     }
 
-    // тест RSA только с шифрованием ключа(поменять с 200 на 32 байта)
+    // тест RSA только с шифрованием AES ключа(32 байта)
     [Benchmark (Description = "Rsa Encrypt Only")]
     public byte[] RsaEncryptOnly()
     {
-        return rsa.Encrypt(data, RSAEncryptionPadding.Pkcs1);
+        return rsa.Encrypt(RSAdata, RSAEncryptionPadding.Pkcs1);
     }
 
-    // тест RSA c шифрованием и генерацией ключей(поменять с 200 на 32 байта)
+    
+    // тест RSA с шифрованием AES ключа(32 байта) + генерация RSA ключей
     [Benchmark(Description = "Rsa Encrypt With Key Generation")]
     public byte[] RsaEncryptWithKeyGeneration()
     {
@@ -75,7 +92,90 @@ public class FullCryptoBenchmarks
         RSAParameters publickey = rsa.ExportParameters(false);
         rsa.ImportParameters(privatekey);
         rsa.ImportParameters(publickey);
-        return rsa.Encrypt(data, RSAEncryptionPadding.Pkcs1);
+        return rsa.Encrypt(RSAdata, RSAEncryptionPadding.Pkcs1);
+    }
+
+
+    [Benchmark (Description = "ECDH Encrypt Only")]
+    public byte[] ECDHEncryptOnly()
+    {
+        // формируем общий секрет (можно использовать в гибридном алгоритме как ключ для AES)
+        return alice.DeriveKeyFromHash(bobPublicKey, HashAlgorithmName.SHA256);
+    }
+
+    // тест ECDH используя старую кривую P-256
+    [Benchmark (Description = "ECDH Encrypt With Key Generation")]
+    public byte[] ECDHEncryptWithKeyGeneration()
+    {
+        alice=ECDiffieHellman.Create();
+        bob= ECDiffieHellman.Create();
+        bobPublicKey = bob.PublicKey;
+        return alice.DeriveKeyFromHash(bobPublicKey, HashAlgorithmName.SHA256);
+    }
+
+
+    // гибридное шифрование AES+RSA без генерации пары ключей
+    [Benchmark (Description = "HPKE AES+RSA Encrypt Only")]
+    public byte[] HPKE_AES_RSA_EncryptOnly()
+    {
+        var aesKey = aes.Key;
+        var aesIV = aes.IV;
+        aes.EncryptCbc(AESdata, aesIV);
+        
+        return rsa.Encrypt(aesKey, RSAEncryptionPadding.Pkcs1);
+    }
+
+    
+    // гибридное шифрование AES+ECDH с кривой nistP384 без генерации ключей
+    [Benchmark (Description = "HPKE AES+ECDH Encrypt Only")]
+    public byte[] HPKE_AES_ECDH_EncryptOnly()
+    {
+        var aesKey= alice.DeriveKeyFromHash(bobPublicKey, HashAlgorithmName.SHA256);
+        aes.Key = aesKey;
+        var aesIV = aes.IV;
+        
+        return aes.EncryptCbc(AESdata, aesIV);
     }
     
+
+    //полный тест гибридного шифрования AES+RSA
+    [Benchmark (Description = "HPKE AES+RSA Encrypt With Key Generation")]
+    public byte[] HPKE_AES_RSA_EncryptWithKeyGeneration()
+    {
+        aes = Aes.Create();
+        aes.GenerateKey();
+        aes.GenerateIV();
+        iv = aes.IV;
+        aes.EncryptCbc(AESdata, iv);
+        
+        
+        rsa = RSA.Create(2048);
+        
+        RSAParameters privatekey = rsa.ExportParameters(true);
+        RSAParameters publickey = rsa.ExportParameters(false);
+        rsa.ImportParameters(privatekey);
+        rsa.ImportParameters(publickey);
+        
+        return rsa.Encrypt(aes.Key, RSAEncryptionPadding.Pkcs1);
+    }
+
+    
+    // полный тест гибридного шифрования AES+ECDH
+    [Benchmark (Description = "HPKE AES+ECDH Encrypt With Key Generation")]
+    public byte[] HPKE_AES_ECDH_EncryptWithKeyGeneration()
+    {
+        aes = Aes.Create();
+        
+        // меняем кривую
+        alice=ECDiffieHellman.Create(ECCurve.NamedCurves.nistP384);
+        bob= ECDiffieHellman.Create(ECCurve.NamedCurves.nistP384);
+        bobPublicKey = bob.PublicKey;
+        
+        aes.Key=alice.DeriveKeyFromHash(bobPublicKey, HashAlgorithmName.SHA256);
+        aes.GenerateIV();
+        iv = aes.IV;
+        
+        
+        return aes.EncryptCbc(AESdata, iv);
+    }
 }
